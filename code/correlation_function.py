bc6e5e99ff8a3b96c9654250308d03f295538cea
Kate Storey-Fisher <ksf293@nyu.edu>
2019-08-28 13:31:48 -0400

    cf mstellar

--- code/correlation_function.py
+++ code/correlation_function.py
@@ -1,14 +1,25 @@
 import numpy as np
 from Corrfunc.theory.xi import xi
+from Corrfunc.theory.wp import wp
 
 import utils
 
 
-tag = '_abacus_small_rbig'
-halos, arrs = utils.load_halos(tag, cols=['x','y','z'], subsample=1000)
+tag = '_abacus_mstellar_'
+mode = 'wp'
+halos, arrs = utils.load_halos(tag, cols=['x','y','z'])
 X, Y, Z = arrs
 print("Got halo properties")
 
+#Load matched catalog
+print("Loading matched catalog")
+mstellar = np.loadtxt("../catalogs/catalog_mstellar_abacus.dat")
+print(len(mstellar))
+
+X = X[~np.isnan(mstellar)]
+Y = Y[~np.isnan(mstellar)]
+Z = Z[~np.isnan(mstellar)]
+print(len(X))
 if 'abacus' in tag:
     boxsize = 500 #Mpc/h
 elif 'ds14b' in tag:
@@ -17,10 +28,17 @@ else:
     raise ValueError
 nthreads = 24
 nbins = 15
-bins = np.linspace(40.0, 150.0, nbins + 1) 
+bins = np.logspace(np.log10(0.1), np.log10(10.0), nbins + 1) #log
+#bins = np.linspace(0.1, 10.0, nbins + 1) #regular
+#bins = np.linspace(40.0, 150.0, nbins + 1) #rbig
+
 
 print("Running corrfunc")
-xi_counts = xi(boxsize, nthreads, bins, X, Y, Z)
+if mode=='xi':
+    res = xi(boxsize, nthreads, bins, X, Y, Z)
+elif mode=='wp':
+    pimax = 40.0
+    res = wp(boxsize, pimax, nthreads, bins, X, Y, Z)
 print("Ran corrfunc")
 
-np.save('../results/xi{}.npy'.format(tag), np.array([bins, xi_counts]))
+np.save('../results/{}{}.npy'.format(mode, tag), np.array([bins, res]))
